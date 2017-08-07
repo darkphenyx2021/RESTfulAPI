2b5d97a5d138de86fa8d2071f3b4c1104edb14db
Josh fair <phenyx2021@gmail.com>
2017-08-06 22:16:05 -0400

    OAuth2 implemented and mostly done with provisions and gates to further restrict access to the api endpoints

--- app/Http/Controllers/Seller/SellerCategoryController.php
+++ app/Http/Controllers/Seller/SellerCategoryController.php
@@ -11,10 +11,14 @@ class SellerCategoryController extends ApiController
     public function __construct()
     {
         parent::__construct();
+        $this->middleware('scope:read-general')->only(['index']);
+        $this->middleware('can:view,seller')->only(['index']);
     }
+
     /**
      * Display a listing of the resource.
      *
+     * @param Seller $seller
      * @return \Illuminate\Http\Response
      */
     public function index(Seller $seller)
